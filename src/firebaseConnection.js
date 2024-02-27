70c7fe754eedc0fdbc95f300b739a97254502295
jonata.silva <jonata.silva@izatta.tech>
2024-02-27 16:42:05 -0300

    Configurado conexao firebase

---
+++ src/firebaseConnection.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCih7FgyHC41ZycjBAFx2QCqXDZ7WZMDrQ",
+    authDomain: "curso-fd3be.firebaseapp.com",
+    projectId: "curso-fd3be",
+    storageBucket: "curso-fd3be.appspot.com",
+    messagingSenderId: "220722696866",
+    appId: "1:220722696866:web:938aa3ea3863fe47a7fdfb",
+    measurementId: "G-1GTDK17014"
+  };
+
+  const firebaseApp = initializeApp(firebaseConfig);
+
+  const db = getFirestore(firebaseApp);
+
+  export { db };
